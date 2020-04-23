1020f0ef13caf25acbeaf8a8736c6871efbcb85e
Saurabh Rajput <saurabh.rajput@tatvasoft.com>
2020-04-23 10:56:04 +0530

    Initial commit

---
+++ EmiCalculation/EmiCalculation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmiCalculation
+//
+//  Created by PCQ184 on 23/04/20.
+//  Copyright © 2020 PCQ184. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
